c9f8135e50185f93e2a5d9e407019e8f5cbe3321
Matti Syrjänen <mhsyrjanen@outlook.com>
2021-04-27 22:50:51 +0300

    Removed sorting of columns as unneccessary operation, implemented a printing function

--- pc_harjoitus.py
+++ pc_harjoitus.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 from itertools import product
+import math
 
 omega = {
     0: (1, 2, -3, 4),
@@ -23,14 +24,10 @@ def xro2Matrix(omega, alpha):
         var for key, variables in omega.items() for var in variables
     }
 
-    # Sort the variables in the correct column order. The negations should
-    # appear after the corresponding literal, i.e. 1, 2, 3, -3, 4, 5, -5, 6, 7
-    allVariablesSorted = sorted(allVariables, key=abs)
-
     # Store the column indexes in a dictionary for faster access
     # (constant time). Using the list function index() would be O(n).
     columns = {
-        var: col for col, var in enumerate(allVariablesSorted)
+        var: col for col, var in enumerate(allVariables)
     }
 
     columnCount = len(columns) + 1 # Add one for the alpha column
@@ -46,7 +43,7 @@ def xro2Matrix(omega, alpha):
         row[alphaColumn] = 1 if alpha[rowIdx] else 0
         A.append(row)
         
-    return A
+    return A, columns
         
 def replaceRowValues(A, cols, targetRow, otherRow):
     '''
@@ -62,7 +59,7 @@ def rredMod2(A):
     elimination. This function only considers matrixes with binary values.
     '''
 
-    if A == None or A == []:
+    if not A:
         return
 
     B = deepcopy(A)
@@ -116,7 +113,7 @@ def xSols(B):
     and so on.
     '''
 
-    if B == None or B == []:
+    if not B:
         return [], False
 
     pivotCols = []
@@ -175,4 +172,55 @@ def xSols(B):
 
     return X, True
 
-print(xSols(rredMod2(xro2Matrix(omega, alpha))))
+def getNumberOfChars(number):
+    '''
+    Helper function which returns the number of characters in a number
+    '''
+
+    if number > 0:
+        return int(math.log10(number)) + 1
+    if number < 0:
+        return int(math.log10(-number)) + 2
+    return 1 # number is 0
+
+def printSolutions(solutionsMatrix, columns):
+    '''
+    Helper function which prints the solution in a clearer form
+    '''
+
+    if not solutionsMatrix:
+        return
+
+    variables = []
+    maxSpacing = 1
+
+    for var, index in columns.items():
+        variables.insert(index, var)
+        numberOfChars = getNumberOfChars(var)
+
+        if numberOfChars > maxSpacing:
+            maxSpacing = numberOfChars
+
+    variablesCount = len(variables)
+    solutionsCount = len(solutionsMatrix[0])
+
+    for var in variables:
+        print(var, end=' ' * (maxSpacing - getNumberOfChars(var) + 1))
+    print('\n')
+
+    for i, var in enumerate(variables):
+        numberOfChars = getNumberOfChars(var)
+        spaceLine = '' if i == len(variables) - 1 else '-' * (maxSpacing - numberOfChars + 1)
+        print('-' * numberOfChars, end=spaceLine)
+    print('\n')
+
+    spacing = ' ' * maxSpacing
+
+    for i in range(solutionsCount):
+        for j in range(variablesCount):
+            print(solutionsMatrix[j][i], end=spacing)
+        print('\n')
+
+augmentedBinaryMatrix, columns = xro2Matrix(omega, alpha)
+solution = xSols(rredMod2(augmentedBinaryMatrix))
+printSolutions(solution[0], columns)
